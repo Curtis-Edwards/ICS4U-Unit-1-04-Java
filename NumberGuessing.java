fc35559d63700e325ca572cad6d16fb95e350ff4
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-02-26 16:55:26 -0500

    Create NumberGuessing.java

---
+++ NumberGuessing.java
@@ -0,0 +1,72 @@
+/*
+ * This is a program creates a random number 
+ * that the user tries to guess
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-02-26
+ */
+
+import java.util.Scanner;
+
+/**
+* This is the standard "NumberGuessing" program.
+*/
+final class NumberGuessing {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private NumberGuessing() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(String[] args) {
+        // constants & variables
+        final int randomNumber = random.nextInt(6) + 1;
+        int totalGuesses = 0;
+        boolean validInput = false;
+
+        while (1) {
+            // input and error check
+            try {
+                System.out.print("Guess a number between 1 - 6: ");
+                final String userGuessString = scanner.nextLine().trim();
+                if (objectMassString.isEmpty()) {
+                    System.out.println("Invalid input. (This won’t count as a guess)\n");
+                } else {
+                    final userGuessInt = Integer.parseInt(userGuessString);
+                    validInput = true;
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid input. (This won’t count as a guess)\n");
+            }
+            // process
+            if (validInput) {
+                if (userGuessInt > randomNumber) {
+                    totalGuesses += 1;
+                    console.log("You guessed too high, try again!\n");
+                } else if (userGuessInt < randomNumber) {
+                    totalGuesses += 1;
+                    console.log("You guessed too low, try again!\n");
+                } else {
+                    // output
+                    totalGuesses += 1;
+                    console.log("Correct!\nTotal guesses: " + totalGuesses);
+                    break;
+                }
+            }
+        }
+    System.out.println("\nDone.");
+    }
+}
